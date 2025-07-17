6d762d065fa260ad03a5f6b1281bf2c2257e52fe
Rafael Harthopf Pereira <harthopfpereira96@gmail.com>
2025-07-17 20:42:30 -0300

    variavel estava errado

--- src/middlewares/securityApi.ts
+++ src/middlewares/securityApi.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export function checkApiKey(req: Request, res: Response, next: NextFunction) {
     const apiKey = req.headers['x-api-key'];
 
-    if (!apiKey || apiKey !== '65b34eab8b65512dfe5807d654cd9c3e1a72cf06f7a8841c573a28ee3a292de5') {
+    if (!apiKey || apiKey !== process.env.API_KEY) {
         return res.status(403).json({ message: 'Acesso negado. API Key inválida ou ausente.' });
     }
 
